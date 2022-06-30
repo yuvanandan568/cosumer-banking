0803188141ab424b3f20a841bfbdf2720671f7a8
yuvanandan568 <107174370+yuvanandan568@users.noreply.github.com>
2022-06-30 16:59:15 +0530

    http://13.233.128.71:8080/jenkins

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new change..poll scm change" );
+        System.out.println( "github hook change" );
     }
 }
